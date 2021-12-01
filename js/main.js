1f0bab24c5395874749d8f5f7d4c5bef16f4fa2f
Mixazpp <92872736+Mixazpp@users.noreply.github.com>
2021-12-01 08:41:54 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,34 @@
+$(function(){
+    $('.popap__link').magnificPopup({
+		disableOn: 700,
+		type: 'iframe',
+		mainClass: 'mfp-fade',
+		removalDelay: 160,
+		preloader: false,
+		fixedContentPos: false
+	});
+	$('.gallery__slider').slick({
+		prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/errow-left.svg" alt="" ></button>',
+		nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/errow-right.svg" alt="" ></button>',
+	});
+
+
+	$('.gallery__item').magnificPopup({
+		delegate: 'a',
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		
+	});
+
+	$('.menu__btn').on('click', function(){
+		$('.menu__list').toggleClass('menu__list--active');
+	})
+
+
+});
